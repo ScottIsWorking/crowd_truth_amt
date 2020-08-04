49bfc505753d000ea2b827d73e73695afff9c69b
ScottIsWorking <scott@avalancheinsights.com>
2020-08-04 15:09:23 -0500

    setup.py uses requirements.txt

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@
 
 from setuptools import setup
 
+with open('requirements.txt') as fIn:
+    requirements=fIn.read().splitlines()
+
 setup(name='crowdtruth_amt',
       version='0.1',
       description='metrics for use with multilabel turk annotation tasks',
@@ -11,14 +14,4 @@ setup(name='crowdtruth_amt',
       author_email='scott@avalancheinsights.com',
       packages=['crowd_truth_amt'],
       zip_safe=False,
-      install_requires=[efficient-apriori>=1.1.1,
-      json5>=0.9.5,
-      numpy>=1.19.0,
-      pandas>=1.0.5,
-      pandocfilters>=1.4.2,
-      qgrid>=1.3.1,
-      regex>=2020.6.8,
-      scikit-learn>=0.23.1,
-      scipy>=1.5.0,
-      tqdm>=4.46.1,
-      urllib3>=1.25.9])
+      install_requires=requirements)
